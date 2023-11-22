d347dda77fa82661f46513ed6214bae534b1d22c
Lay <371986203@qq.com>
2023-11-22 22:28:49 +0800

    add new little project

---
+++ flutter_chatapp_collectproj/dear-imclient/lib/config.dart
@@ -0,0 +1,11 @@
+
+// ignore_for_file: constant_identifier_names
+
+const String APP_NAME = "iM";
+// const String HOST = "10.242.142.129";
+// const String HOST = "192.168.31.230"; // mac
+// const String HOST = "192.168.31.37"; // windows
+const String HOST = "192.168.1.1";//服务地址
+
+//native channel name
+const String NATIVE_CHANNEL = "dearim_channel";
